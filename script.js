84d1014ae551d6a3d85c66536e1da8ef7f96e3ab
Daniel <daniel.lam3987@gmail.com>
2024-07-01 16:30:45 -0400

    Added basic html and css. Updated amount of cash in the register

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let price = 1.87;
 let cid = [
-  ['PENNY', 0.01],
-  ['NICKEL', 0.05],
-  ['DIME', 0.1],
-  ['QUARTER', 0.25],
-  ['ONE', 1],
-  ['FIVE', 5],
-  ['TEN', 10],
-  ['TWENTY', 20],
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 5],
+  ['FIVE', 10],
+  ['TEN', 20],
+  ['TWENTY', 60],
   ['ONE HUNDRED', 100]
 ];
 let cash;
